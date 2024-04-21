65bab465f87e9858221169af2e79275debd51949
KadoProG <splatrain@gmail.com>
2024-04-21 15:06:50 +0900

    update: context経由でカラーモードを変更できるように実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
 'use client';
 
 import { Box, Button, Paper, Stack } from '@mui/material';
+import { useColorModeContext } from '@/libs/theme/ThemeRegistry';
 
-export default function Home() {
+const Home = () => {
+  const { toggleColorMode, selectedMode } = useColorModeContext();
   return (
     <Paper component={Box} p={2}>
       <Stack spacing={1}>
-        <Button variant="outlined">ライト</Button>
-        <Button variant="outlined">システム</Button>
-        <Button variant="outlined">ダーク</Button>
+        <Button
+          variant={selectedMode === 'light' ? 'contained' : 'outlined'}
+          onClick={() => toggleColorMode('light')}
+        >
+          ライト
+        </Button>
+        <Button
+          variant={selectedMode === 'device' ? 'contained' : 'outlined'}
+          onClick={() => toggleColorMode('device')}
+        >
+          システム
+        </Button>
+        <Button
+          variant={selectedMode === 'dark' ? 'contained' : 'outlined'}
+          onClick={() => toggleColorMode('dark')}
+        >
+          ダーク
+        </Button>
       </Stack>
     </Paper>
   );
-}
+};
+export default Home;
